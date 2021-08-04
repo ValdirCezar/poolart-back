be629e991ed93d168cbf28a33125fea4877ef006
ValdirCezar <valdircezar312@gmail.com>
2021-08-04 20:49:48 -0300

    feat: add address class

---
+++ src/main/java/com/valdir/poolart/Address.java
@@ -0,0 +1,50 @@
+package com.valdir.poolart;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.valdir.poolart.domain.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.Hibernate;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "TB_ADDRESS")
+public class Address  implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String cep;
+    private String Country;
+    private String city;
+    private String street;
+    private String number;
+
+    @JsonIgnore
+    @OneToOne
+    @JoinColumn
+    protected User user;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Address address = (Address) o;
+
+        return Objects.equals(id, address.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return 1395783287;
+    }
+}

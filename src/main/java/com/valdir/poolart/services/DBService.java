1422fa2f9682b04bddc394538afc8d745789951b
ValdirCezar <valdircezar312@gmail.com>
2021-08-04 12:51:04 -0300

    feat: add test profile with h2 database

--- src/main/java/com/valdir/poolart/services/DBService.java
+++ src/main/java/com/valdir/poolart/services/DBService.java
@@ -1,8 +1,13 @@
 package com.valdir.poolart.services;
 
+import com.valdir.poolart.domain.Artist;
+import com.valdir.poolart.domain.enums.PersonType;
+import com.valdir.poolart.domain.enums.Profile;
 import com.valdir.poolart.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
 
 @Service
 public class DBService {
@@ -11,6 +16,8 @@ public class DBService {
     private UserRepository userRepository;
 
     public void startDB() {
+        Artist a1 = new Artist(null, "Valdir Cezar", PersonType.PHYSICAL, "43984634308", "valdr@mail.com", "123", Profile.ADMIN, "582.579.570-70", "Cantor", 21);
 
+        userRepository.saveAll(Arrays.asList(a1));
     }
 }

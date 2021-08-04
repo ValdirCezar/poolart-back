b487eb4d59ba233cf7778250c7de59088b7bf295
ValdirCezar <valdircezar312@gmail.com>
2021-08-04 20:50:15 -0300

    refact: update db service

--- src/main/java/com/valdir/poolart/services/DBService.java
+++ src/main/java/com/valdir/poolart/services/DBService.java
@@ -1,10 +1,12 @@
 package com.valdir.poolart.services;
 
+import com.valdir.poolart.Address;
 import com.valdir.poolart.domain.Artist;
 import com.valdir.poolart.domain.Enterprise;
 import com.valdir.poolart.domain.Offer;
 import com.valdir.poolart.domain.enums.Profile;
 import com.valdir.poolart.domain.enums.Status;
+import com.valdir.poolart.repositories.AddressRepository;
 import com.valdir.poolart.repositories.OfferRepository;
 import com.valdir.poolart.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,11 +24,17 @@ public class DBService {
     @Autowired
     private OfferRepository offerRepository;
 
+    @Autowired
+    private AddressRepository addressRepository;
+
     public void startDB() {
         Artist a1 = new Artist(null, "Valdir Cezar", "43984634308", "valdr@mail.com", "123", Profile.ADMIN, "582.579.570-70", "Cantor", 21);
         a1.addSkill("Sabe programar cantando kkk");
 
         Enterprise e1 = new Enterprise(null, "ZACK BURGUER", "43991518434", "zack@mail.com", "123", Profile.ENTERPRISE, "Amburgueria do Matheus", "07360730000142");
+
+        Address address1 = new Address(null, "86086086", "Brasil", "Londrina", "Rua dos teste", "999", a1);
+        Address address2 = new Address(null, "86086086", "Brasil", "Londrina", "Rua dos teste", "999", e1);
 
         Offer o1 = new Offer(null, "Offerta 1", LocalDate.now(), 125.5F, Status.OPEN, "Observações 01", e1);
         Offer o2 = new Offer(null, "Offerta 2", LocalDate.now(), 125.5F, Status.IN_PROGRESS, "Observações 02", e1);
@@ -35,5 +43,6 @@ public class DBService {
 
         userRepository.saveAll(Arrays.asList(a1, e1));
         offerRepository.saveAll(Arrays.asList(o1, o2, o3, o4));
+        addressRepository.saveAll(Arrays.asList(address1, address2));
     }
 }

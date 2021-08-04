955d4868c76fa12c08f9834b8b0d6c9c31518425
ValdirCezar <valdircezar312@gmail.com>
2021-08-04 20:49:35 -0300

    feat: add address repository interface

---
+++ src/main/java/com/valdir/poolart/repositories/AddressRepository.java
@@ -0,0 +1,9 @@
+package com.valdir.poolart.repositories;
+
+import com.valdir.poolart.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AddressRepository extends JpaRepository<Address, Integer> {
+}

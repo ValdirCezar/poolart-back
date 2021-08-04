25bc246b60d124930706ca6838f85e45ac53befe
ValdirCezar <valdircezar312@gmail.com>
2021-08-03 22:18:03 -0300

    feat: initial commit

---
+++ src/main/java/com/valdir/poolart/PoolartApplication.java
@@ -0,0 +1,13 @@
+package com.valdir.poolart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoolartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PoolartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/valdir/poolart/PoolartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valdir.poolart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoolartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

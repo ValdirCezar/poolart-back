b340a6d76910161486ff062b2611ff735e67fc0e
ValdirCezar <valdircezar312@gmail.com>
2021-08-05 22:44:12 -0300

    feat: add bean of model mapper

---
+++ src/main/java/com/valdir/poolart/config/ModelMapperConfig.java
@@ -0,0 +1,14 @@
+package com.valdir.poolart.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+
+    @Bean
+    public ModelMapper mapper() {
+        return new ModelMapper();
+    }
+}

f61476db5a240d8761b712fc10629f51b445eb70
Valdir Cezar <valdircezar312@gmail.com>
2021-09-09 21:23:32 -0300

    feat: authenticate adjusts

--- src/main/java/com/valdir/poolart/config/SecurityConfig.java
+++ src/main/java/com/valdir/poolart/config/SecurityConfig.java
@@ -5,8 +5,8 @@ import com.valdir.poolart.security.JWTUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -18,100 +18,49 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
-import java.util.List;
-
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    /**
-     * Environment é uma interface que representa o ambiente no qual
-     * o aplicativo atual está sendo executado. Ele pode ser usado
-     * para obter perfis e propriedades do ambiente do aplicativo.
-     */
-    @Autowired
-    private Environment environment;
+    private static final String[] PUBLIC_MATCHERS = { "/h2-console/**" };
 
+    @Autowired
+    private Environment env;
+    @Autowired
+    private JWTUtil jwtUtil;
     @Autowired
     private UserDetailsService userDetailsService;
 
-    @Autowired
-    private JWTUtil jwtUtil;
-
-    private static final String[] PUBLIC_MATCHERS = {"/h2-console/**"};
-    private static final String[] PUBLIC_MATCHERS_POST = {"/artists/**", "/enterprises/**"};
-
-    /**
-     * Qualquer endpoint que requeira defesa
-     * contra vulnerabilidades comuns pode ser
-     * especificado aqui, incluindo as públicas.
-     */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
-        /*
-          Verificando os profiles ativos. Caso o perfil
-          ativo contenha test o acesso ao h2 será liberado
-         */
-        if(Arrays.asList(environment.getActiveProfiles()).contains("test")) {
+        if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
             http.headers().frameOptions().disable();
         }
 
-        // Habilita as configurações de cors e desabilita a proteção csrf
         http.cors().and().csrf().disable();
-        // Registrando o filtro de authenticação JWT
         http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+        http.authorizeRequests().antMatchers(PUBLIC_MATCHERS).permitAll().anyRequest().authenticated();
 
-        http.authorizeRequests()
-                .antMatchers(PUBLIC_MATCHERS).permitAll()
-                .antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
-                .anyRequest().authenticated();
-
-        // Assegurando que não será criada sessão de usuário
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
-    /**
-     * Como será usada a autenticação do framework
-     * esse metodo deve ser sobrescrito para informarmos
-     * quem será o UserDetailsService que estamos usando
-     * e quem é o algoritmo de codificação que no caso é
-     * o BCryptPasswordEncoder. Usado pela implementação
-     * padrão de authenticationManager()para tentar obter
-     * um AuthenticationManager
-     */
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        // A partir do auth eu digo quem é o UserDetailsService
-        auth.userDetailsService(userDetailsService)
-                // E digo quem é meu algoritmo de encriptação
-                .passwordEncoder(bCryptPasswordEncoder());
+        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
     }
 
-    /**
-     * Configura o cors para receber requisições
-     * de multiplas fontes
-     *
-     * @return source
-     */
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedMethods(Arrays.asList("POST", "PUT", "GET", "DELETE"));
-        configuration.setAllowedOrigins(List.of("*"));
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
+        configuration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "OPTIONS"));
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
 
-    /**
-     * Bean criado para disponibilizar um BCrypt
-     * para encodar a senha do usuario que possa
-     * ser injetado em qualquer classe do sistema
-     * @return new BCryptPasswordEncoder
-     */
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
 }
--- src/main/java/com/valdir/poolart/security/JWTAuthenticationFilter.java
+++ src/main/java/com/valdir/poolart/security/JWTAuthenticationFilter.java
@@ -9,112 +9,64 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
 
-/**
- * Quando eu crio uma classe que extende
- * UsernamePasswordAuthenticationFilter automaticamente
- * o spring vai entender que esse filtro vai interceptar
- * a requisição POST no endpoint /login que inclusive
- * é o um endpoint reservado do Spring Security
- */
 public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
-    /**
-     * AuthenticationManager é a principal interface de
-     * estratégia para autenticação.
-     * Se o principal da autenticação de entrada for válido e verificado,
-     * o metodo authenticate retorna uma instância de Authentication com
-     * o sinalizador de autenticado definido como verdadeiro. Do contrário,
-     * se o principal não for válido, ele lançará uma AuthenticationException.
-     * Para o último caso, ele retorna nulo se não puder decidir.
-     */
-    private final AuthenticationManager authenticationManager;
-    private final JWTUtil jwtUtil;
+    private AuthenticationManager authenticationManager;
+    private JWTUtil jwtUtil;
 
     public JWTAuthenticationFilter(AuthenticationManager authenticationManager, JWTUtil jwtUtil) {
+        super();
         this.authenticationManager = authenticationManager;
         this.jwtUtil = jwtUtil;
     }
 
-    /**
-     * Metodo que irá tentar autenticar
-     */
     @Override
-    public Authentication attemptAuthentication(HttpServletRequest request,
-                                                HttpServletResponse response) throws AuthenticationException {
-
-        /*
-          Vamos pegar os valores passados na requisição POST para
-          o endpoint /login, convertê-los em CredentialsDTO
-          instanciar um objeto do tipo UsernamePasswordAuthenticationToken
-          passá-lo como parâmetro para o metodo authenticate que irá
-          tentar realizar a autenticação. O framework fará isso usando
-          os contratos implementados em UserDetails, UserDetailsService
-          de forma automática
-         */
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+            throws AuthenticationException {
         try {
-            /* getInputStream() Recupera o corpo da solicitação como dados
-               binários usando um ServletInputStream. Este método ou getReader
-               pode ser chamado para ler o corpo */
             CredentialsDTO creds = new ObjectMapper().readValue(request.getInputStream(), CredentialsDTO.class);
-
-            /* Criando esse objeto para passar para o metodo autenticate do authenticationManager
-               verificar se o usuario e senha passados na requisição são válidos */
             UsernamePasswordAuthenticationToken authenticationToken =
                     new UsernamePasswordAuthenticationToken(creds.getEmail(), creds.getPassword(), new ArrayList<>());
-
-            /* Esse é o metodo que verifica se o usuario e senha passados são válidos */
             return authenticationManager.authenticate(authenticationToken);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    /**
-     * Caso a tentativa de autenticação ocorrer com
-     * sucesso esse metodo será chamado
-     */
     @Override
-    protected void successfulAuthentication(HttpServletRequest request,
-                                            HttpServletResponse response,
-                                            FilterChain chain,
-                                            Authentication authResult) {
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+                                            Authentication authResult) throws IOException, ServletException {
 
         String username = ((UserSS) authResult.getPrincipal()).getUsername();
         String token = jwtUtil.generateToken(username);
-        response.addHeader("Authorization", "Bearer " + token);
+        response.setHeader("access-control-expose-headers", "Authorization");
+        response.setHeader("Authorization", "Bearer " + token);
     }
 
-    /**
-     * Esse metodo irá ser chamado caso não ocorra a autenticação
-     */
     @Override
-    protected void unsuccessfulAuthentication(HttpServletRequest request,
-                                              HttpServletResponse response,
-                                              AuthenticationException failed) throws IOException {
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response,
+                                              AuthenticationException failed) throws IOException, ServletException {
+
         response.setStatus(401);
         response.setContentType("application/json");
         response.getWriter().append(json());
-
     }
 
-    /**
-     * Metodo onde escrevemos um json para retornar como erro
-     * de credenciais para o usuario que digitar email e senha
-     * incorretos na autenticação. Esse metodo será usado pelo
-     * metodo unsuccessfulAuthentication acima.
-     */
-    private String json() {
+    private CharSequence json() {
         long date = new Date().getTime();
-        return "{\"timestamp\": " + date + ", "
+        return "{"
+                + "\"timestamp\": " + date + ", "
                 + "\"status\": 401, "
                 + "\"error\": \"Não autorizado\", "
                 + "\"message\": \"Email ou senha inválidos\", "
                 + "\"path\": \"/login\"}";
     }
+
 }
